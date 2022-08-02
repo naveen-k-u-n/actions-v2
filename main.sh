5e0ef8b2403b915d754aaf2d19fbfecfc81994ed
naveen-k-u-n <naveenkundeti11@gmail.com>
2022-08-02 19:07:00 +0530

    test stale old sh

--- main.sh
+++ main.sh
@@ -1,6 +1,7 @@
 # !/bin/bash
 
 PR_URL="$PR_URL"
+
 token="$GITHUB_TOKEN"
 BASE_URI="https://api.github.com"
 owner="$REPO_OWNER"
@@ -18,8 +19,8 @@ stale=120
 close=60
 
 #date and time of PR
-latest_commit_date=$(curl -X GET -u devops-ibs:$token $BASE_URI/repos/$owner/$repo/pulls/$pull_number/commits | jq -r '.[-1].commit.committer.date')
-stale_date=$(curl -X GET -u devops-ibs:$token $BASE_URI/repos/$owner/$repo/pulls/$pull_number | jq -r '.updated_at')
+latest_commit_date=$(curl -X GET -u $owner:$token $BASE_URI/repos/$owner/$repo/pulls/$pull_number/commits | jq -r '.[-1].commit.committer.date')
+stale_date=$(curl -X GET -u $owner:$token $BASE_URI/repos/$owner/$repo/pulls/$pull_number | jq -r '.updated_at')
 
 live_date=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 convert_live_date=$(date -u -d "$live_date" +%s)
@@ -41,18 +42,22 @@ echo "label difference time: $label_diff"
 if [ $DIFFERENCE -lt $active ]
 then
    echo "This PR is active. Don't close PR"
-   gh pr edit $PR_URL --remove-label "Stale"
 elif [ $DIFFERENCE -le $stale ]
 then
    echo "This PR is stale because it has been open 10 days with no activity."
-   gh pr edit $PR_URL --add-label "Stale" 
-   gh pr comment $PR_URL --body "This issue is stale because it has been open 10 days with no activity. Remove stale label or comment or this will be closed in 4 days."
+   curl -X POST -u $owner:$token $label \
+  -d '{ "labels":["Stale"] }'
+
+  curl -X POST -u $owner:$token $comments_url \
+  -d '{"body":"This PR is stale because it has been open 15 days with no activity. Remove stale label or comment or this will be closed in 2 days."}' 
 elif [ $label_diff -gt $close ]
 then
    echo "This PR was closed because it has been stalled for 4 days with no activity."
-   gh pr close $PR_URL
-   gh pr edit $PR_URL --remove-label "Stale"
-   gh pr comment $PR_URL --body "This PR was closed because it has been stalled for 4 days with no activity."
+    curl -X PATCH -u $owner:$token $pr_number \
+  -d '{ "state": "closed" }'
+
+  curl -X POST -u $owner:$token $comments_url \
+  -d '{"body":"This PR was closed because it has been stalled for 2 days with no activity."}'
  
 else
    echo "None of the condition met"
