109dcde2655801878c69276302a8fdfd9cd666c3
naveen-k-u-n <naveenkundeti11@gmail.com>
2022-08-02 13:19:02 +0530

    only stale

--- main.sh
+++ main.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-MERGE_PR="$MERGE_PR"
-CLOSE_PR="$CLOSE_PR"
-PR_DESCRIPTION="$PR_DESCRIPTION"
-BASE="$BASE_REF"
-HEAD="$HEAD_REF"
-SEP="&&"
 STALE_DAYS=$STALE_DAYS
 CLOSE_DAYS=$CLOSE_DAYS
 
@@ -49,8 +43,7 @@ echo "Days Before Close in seconds: $STALE_CLOSE"
 case $((
 (DIFFERENCE < STALE_LABEL) * 1 +
 (DIFFERENCE > STALE_LABEL) * 2 +
-(DIFFERENCE > STALE_CLOSE) * 3 +
-(pr_number = null) * 4)) in
+(DIFFERENCE > STALE_CLOSE) * 3)) in
 (1) echo "This PR is active."
 ;;
 (2) echo "This PR is Stale."
@@ -68,59 +61,9 @@ case $((
   curl -X POST -u $owner:$token $comments_url \
   -d '{"body":"This PR was closed because it has been stalled for 2 days with no activity."}'
 ;;
-(4) echo "No active PRs"
-;;  
+ 
 esac  
 
 }
 
-# Issue comments
-merge() {
-case "${MERGE_PR}" in
-  "true") 
-  echo "PR has Approved."
-  curl -X PUT -u $owner:$token $BASE_URI/repos/$repo/pulls/$pull_number/merge \
-  -d '{ "merged": true }'
-  curl -X POST -u $owner:$token $BASE_URI/repos/$repo/issues/$pull_number/comments \
-  -d '{"body":"Pull Request Merged!"}'
-    ;;
-esac
-}
-
-close() {
-case "${CLOSE_PR}" in
-  "true") 
-  echo "PR has Closed manually by comments."
-  curl -X PATCH -u $owner:$token $BASE_URI/repos/$repo/pulls/$pull_number \
-  -d '{ "state": "closed" }'
-  curl -X POST -u $owner:$token $BASE_URI/repos/$repo/issues/$pull_number/comments \
-  -d '{"body":"Pull Request Closed!"}'
-    ;;
-esac
-}
-
-# Pull_request target master
-target() {
-case "${BASE}${SEP}${HEAD}" in
-  "true${SEP}false") 
-    curl -X PATCH -u $owner:$token $BASE_URI/repos/$repo/pulls/$pull_number \
-  -d '{ "state": "closed" }'
-    curl -X POST -u $owner:$token $BASE_URI/repos/$repo/issues/$pull_number/comments \
-  -d '{"body":"Do not accept PR target from feature branch to master branch."}'
-    ;;
-esac
-}
-
-# Description
-description() {
-case "$PR_DESCRIPTION" in
-  "true") 
-    echo "PR has No valied description" 
-    curl -X POST -u $owner:$token $BASE_URI/repos/$repo/issues/$pull_number/comments \
-    -d '{"body":"No Description on PR body. Please add valid description."}'
-    curl -X PATCH -u $owner:$token $BASE_URI/repos/$repo/pulls/$pull_number \
-    -d '{ "state": "closed" }'
-  ;;
-esac  
-}
 "$@"
