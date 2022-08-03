fd0e4287d727e0c97c092e259ba214985a374e9e
naveen-k-u-n <naveenkundeti11@gmail.com>
2022-08-03 18:30:31 +0530

    stale-close-2

--- main-one.sh
+++ main-one.sh
@@ -13,36 +13,51 @@ pull_number="$PR_NUMBER"
 # Stale Pull Request
 stale-close() {
 
+pr_created_at=$(curl -X GET -u $owner:$token $BASE_URI/repos/$repo/pulls | jq -r '.[-1].created_at')
 pr_updated_at=$(curl -X GET -u $owner:$token $BASE_URI/repos/$repo/pulls | jq -r '.[-1].updated_at')
 
 pr_number=$(curl -X GET -u $owner:$token $BASE_URI/repos/$repo/pulls | jq -r '.[-1].url')
 comments_url=$(curl -X GET -u $owner:$token $BASE_URI/repos/$repo/pulls | jq -r '.[-1].comments_url')
 label=$(curl -X GET -u $owner:$token $BASE_URI/repos/$repo/issues | jq -r '.[-1].url')
-# label_date=$(curl -X GET -u $owner:$token $BASE_URI/repos/$repo/issues/$pr_number/labels | jq -r '.[-1].name')
+
+
+label_created_at=$(curl -X GET -u $owner:$token $BASE_URI/repos/$repo/issues/$PR_NUMBER/timeline | jq -r '.[-1].created_at')
+timeline_label=$(curl -X GET -u $BASE_URI/repos/$repo/issues/$PR_NUMBER | jq -r '.[] | select( .label.name == "Stale" )')
+
+# echo "live date: $live_date"
+# echo "convert live date: $convert_live_date"
+echo "pr created at: $pr_created_at"
+echo "pr updated at: $pr_updated_at"
+echo "label created at timeline: $label_created_at"
+echo "label name: $timeline_label"
+
 
 live_date=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 convert_live_date=$(date -u -d "$live_date" +%s)
 convert_pr_updated_at=$(date -u -d "$pr_updated_at" +%s)
+convert_label_created_at=$(date -u -d "$label_created_at" +%s)
 DIFFERENCE=$((convert_live_date - convert_pr_updated_at))
+DIFFERENCE_LABEL=$((convert_live_date - convert_label_created_at))
 SECONDSPERDAY=86400
-# STALE_LABEL=$(( STALE_DAYS * SECONDSPERDAY ))
-# STALE_CLOSE=$(( CLOSE_DAYS * SECONDSPERDAY ))
-STALE_LABEL=120
-STALE_CLOSE=160
+MIN_LABEL=120
+MAX_LABEL=160
 
 
 echo "live date: $live_date"
 echo "convert live date: $convert_live_date"
 echo "pr updated at: $pr_updated_at"
-echo "convert pr updated date: $convert_pr_updated_at"  
+echo "convert pr updated at: $convert_pr_updated_at" 
+echo "label created at: $label_created_at" 
+echo "convert label created at: $convert_label_created_at"  
 echo "difference time: $DIFFERENCE"
+echo "difference time: $DIFFERENCE_LABEL"
 echo "pr number: $pr_number"
-echo "Days Before Stale in seconds: $STALE_LABEL"
-echo "Days Before Close in seconds: $STALE_CLOSE"
+echo "Days Before Stale in seconds: $MIN_LABEL"
+echo "Days Before Close in seconds: $MIN_LABEL"
 
 case $((
-(DIFFERENCE >= 0 && DIFFERENCE <= STALE_LABEL) * 1 +
-(DIFFERENCE > STALE_CLOSE) * 2)) in
+(DIFFERENCE_LABEL <= MIN_LABEL) * 1 +
+(DIFFERENCE_LABEL > MAX_LABEL) * 2)) in
 (1) echo "This PR is active."
 ;;
 (2) echo "This PR is stale and close"
