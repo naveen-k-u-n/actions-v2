6b2efe0881e2d9ca9e5e53d7961549c3f7351b61
naveen-k-u-n <naveenkundeti11@gmail.com>
2022-09-05 09:10:02 +0530

    add stale-close

--- stale.sh
+++ stale.sh
@@ -135,4 +135,19 @@ then
 fi
 }
 
+comments()
+{
+if [ "$user" = "Bot" ];
+then
+  echo "Dont remove stale label"
+fi
+
+if [ "$user" = "User" ];
+then
+  echo "Remove stale label"
+  curl -X DELETE -u $owner:$token $issue_number/labels \
+  -d '{ "labels":["Stale"] }'
+fi
+}
+
 "$@"
