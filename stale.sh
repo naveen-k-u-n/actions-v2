5bd5e3e52a8a45e1e5215fc3a01956fcdb1838e4
naveen-k-u-n <naveenkundeti11@gmail.com>
2022-09-06 17:27:09 +0530

    changed APIs

--- stale.sh
+++ stale.sh
@@ -18,9 +18,9 @@ comments_url=$(curl -X GET -u $owner:$token $BASE_URI/repos/$repo/pulls | jq -r 
 labels=$(curl -X GET -u $owner:$token $BASE_URI/repos/$repo/issues | jq -r '.[].url')
 
 pr_created_at=$(curl -X GET -u $owner:$token $BASE_URI/repos/$repo/pulls | jq -r '.[].created_at')
-pr_updated_at=$(curl -X GET -u $owner:$token $BASE_URI/repos/$repo/pulls | jq -r '.[].updated_at')
+pr_updated_at=$(curl -X GET -u $owner:$token $BASE_URI/repos/$repo/pulls/$pull_number | jq -r '.[-1].updated_at')
 
-label_created_at=$(curl -X GET -u $owner:$token $issue_number/events | jq -r '.[-1] | select(.event == "labeled") | select( .label.name == "Stale") | .created_at')
+label_created_at=$(curl -X GET -u $owner:$token $BASE_URI/repos/$repo/issues/$pull_number/events | jq -r '.[-1] | select(.event == "labeled") | select( .label.name == "Stale") | .created_at')
 
 
 live_date=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
@@ -70,10 +70,10 @@ then
 
 else [ $UpdatedTime -gt $fifteen_days ]
    echo "This PR is stale because it has been open 15 days with no activity."
-   curl -X POST -u $owner:$token $labels \
+   curl -X POST -u $owner:$token $BASE_URI/repos/$repo/issues/$pull_number/labels \
   -d '{ "labels":["Stale"] }'
 
-  curl -X POST -u $owner:$token $comments_url \
+  curl -X POST -u $owner:$token $BASE_URI/repos/$repo/issues/$pull_number/comments \
   -d '{"body":"This PR is stale because it has been opened 15 days with no activity. Remove stale label or update/comment on PR otherwise this will be closed in 5 days."}' 
 
 fi
@@ -86,16 +86,16 @@ if [ $LabelTime -gt $five_days ]
 then
    echo "This PR is closed because it has been stalled from 5 days"
 
-  curl -X PATCH -u $owner:$token $pr_number \
+  curl -X PATCH -u $owner:$token $BASE_URI/repos/$repo/pulls/$pull_number \
   -d '{ "state": "closed" }'
 
-  curl -X POST -u $owner:$token $comments_url \
+  curl -X POST -u $owner:$token $BASE_URI/repos/$repo/issues/$pull_number/comments \
   -d '{"body":"This PR is closed because it has been stalled from 5 days."}'
 
-  curl -X DELETE -u $owner:$token $issue_number/labels \
+  curl -X DELETE -u $owner:$token $BASE_URI/repos/$repo/issues/$pull_number/labels \
   -d '{ "labels":["Stale"] }'
 
-  curl -X POST -u $owner:$token $labels \
+  curl -X POST -u $owner:$token $BASE_URI/repos/$repo/issues/$pull_number/labels \
   -d '{ "labels":["Stale-Close"] }'
 
 else [ $LabelTime -lt $five_days ]
@@ -122,7 +122,7 @@ prupdate()
 if [ $UpdatedTime -lt $onemin ]
 then
   echo "PR updated. Remove stale label"
-  curl -X DELETE -u $owner:$token $issue_number/labels \
+  curl -X DELETE -u $owner:$token $BASE_URI/repos/$repo/issues/$pull_number/labels \
   -d '{ "labels":["Stale"] }'
 fi
 }
@@ -133,7 +133,7 @@ comments()
 if [ $UpdatedTime -lt $onemin ]
 then
   echo "PR upadted by comments. Remove stale label"
-  curl -X DELETE -u $owner:$token $issue_number/labels \
+  curl -X DELETE -u $owner:$token $BASE_URI/repos/$repo/issues/$pull_number/labels \
   -d '{ "labels":["Stale"] }'
 fi
 }
